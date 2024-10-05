a74b40ba0af52ad127f3683c68577169090bb55f
Scheila Stihler <sstihler@gmail.com>
2024-10-05 18:12:50 -0300

    Resolução do exercício 7

---
+++ LSP.cs
@@ -0,0 +1,32 @@
+namespace Aula3.Exercicios
+{
+public class LSP
+  {
+    //o código da classe abaixo está violando o principio de Liskov. Reescreva aplicando a correção
+  }
+  public interface IForma
+  {
+    int CalcularArea();
+  }
+
+  public class Retangulo : IForma
+  {
+    public int Largura { get; set; }
+    public int Altura { get; set; }
+
+    public int CalcularArea()
+    {
+      return Largura * Altura;
+    }
+  }
+
+  public class Quadrado : IForma
+  {
+    public int Lado { get; set; }
+
+    public int CalcularArea()
+    {
+      return Lado * Lado;
+    }
+  }
+}
